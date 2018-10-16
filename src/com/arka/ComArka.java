7bcaafd77fa9d6c55fbd86eeb729ff84963d1950
User <User@172.16.182.38>
2018-10-16 13:33:19 +0700

    ini adalah percobaan ke 2

--- src/com/arka/ComArka.java
+++ src/com/arka/ComArka.java
@@ -17,6 +17,7 @@ public class ComArka {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world!");
+        System.out.println("World War 3");
     }
     
 }
